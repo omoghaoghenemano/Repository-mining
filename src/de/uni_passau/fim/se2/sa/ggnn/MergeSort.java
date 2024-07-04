ae3bca9ba2340fe8ed67c5eb937c7c2a2a1c2c08
Patric Feldmeier <patric.feldmeier@uni-passau.de>
2024-07-04 07:04:52 +0000

    Exercise-Template pushed by Artemis

---
+++ src/de/uni_passau/fim/se2/sa/ggnn/MergeSort.java
@@ -0,0 +1,17 @@
+package de.uni_passau.fim.se2.sa.ggnn;
+
+import java.util.*;
+
+public class MergeSort {
+
+    /**
+     * Sorts dates with MergeSort.
+     *
+     * @param input the List of Dates to be sorted
+     */
+    public void performSort(final List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
