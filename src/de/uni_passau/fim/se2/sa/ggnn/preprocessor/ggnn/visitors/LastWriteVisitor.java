d12bcefda0b40a0686b5ed2f7ee98a2387c9f967
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-01 22:28:31 +0200

    add last write

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/LastWriteVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/LastWriteVisitor.java
@@ -1,11 +1,15 @@
 package de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors;
 
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.AstNode;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.declaration.MethodDeclaration;
 import de.uni_passau.fim.se2.sa.ggnn.ast.visitor.AstVisitorWithDefaults;
 import de.uni_passau.fim.se2.sa.ggnn.program_graphs.ddg.DataFlowFacts;
+import de.uni_passau.fim.se2.sa.ggnn.program_graphs.ddg.Definition;
+import de.uni_passau.fim.se2.sa.ggnn.program_graphs.ddg.Use;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.IdentityWrapper;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.Pair;
 
+import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Set;
 
@@ -23,5 +27,30 @@ public class LastWriteVisitor implements
         this.dataFlowFacts = dataFlowFacts;
     }
 
-    // TODO: Implement required visitors
+
+    @Override
+    public Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        return data;
+    }
+
+    @Override
+    public Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> visit(MethodDeclaration node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> lastWrites = new HashSet<>();
+
+        for (Pair<Definition, Use> pair : dataFlowFacts.getDefUsePairs()) {
+            IdentityWrapper<AstNode> defNode = astNodeMap.get(pair.a().cfgNode());
+            IdentityWrapper<AstNode> useNode = astNodeMap.get(pair.b().cfgNode());
+            if (defNode != null && useNode != null) {
+                lastWrites.add(new Pair<>(defNode, useNode));
+            }
+        }
+
+        data.addAll(lastWrites);
+        return data;
+    }
+
+
 }
+
+    // TODO: Implement required visitors
+
