ec4318d66da313c13b802e668ae54f3b3c940342
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-02 00:01:02 +0200

    test for addGuardedByEdges

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
@@ -1,6 +1,8 @@
 package de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors;
 
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.AstNode;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.IfStmt;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.WhileStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.visitor.AstVisitorWithDefaults;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.IdentityWrapper;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.Pair;
@@ -19,5 +21,64 @@ public class GuardedByVisitor implements AstVisitorWithDefaults<Void, Set<Pair<I
         this.astNodeMap = astNodeMap;
     }
 
-    // TODO: Implement required visitors
+    @Override
+    public Void defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        // Visit all children nodes
+        for (AstNode child : node.children()) {
+            child.accept(this, data);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(IfStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        IdentityWrapper<AstNode> conditionNode = astNodeMap.get(node.condition());
+
+        if (conditionNode != null) {
+            for (AstNode thenStmt : node.thenStmt().members()) {
+                addGuardedByEdges(conditionNode, thenStmt, data);
+            }
+            if (node.elseStmt() != null) {
+                for (AstNode elseStmt : node.elseStmt().get().members()) {
+                    addGuardedByEdges(conditionNode, elseStmt, data);
+                }
+            }
+        }
+
+        // Visit children nodes
+        node.condition().accept(this, data);
+        node.thenStmt().accept(this, data);
+
+
+        return null;
+    }
+
+    @Override
+    public Void visit(WhileStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        IdentityWrapper<AstNode> conditionNode = astNodeMap.get(node.condition());
+
+        if (conditionNode != null) {
+            for (AstNode bodyStmt : node.block().members()) {
+                addGuardedByEdges(conditionNode, bodyStmt, data);
+            }
+        }
+
+        // Visit children nodes
+        node.condition().accept(this, data);
+        node.block().accept(this, data);
+
+        return null;
+    }
+
+    private void addGuardedByEdges(IdentityWrapper<AstNode> conditionNode, AstNode guardedNode, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        IdentityWrapper<AstNode> guardedWrapper = astNodeMap.get(guardedNode);
+        if (guardedWrapper != null) {
+            data.add(new Pair<>(conditionNode, guardedWrapper));
+            // Visit the guarded node to continue traversal
+            guardedNode.accept(this, data);
+        }
+    }
 }
+
+    // TODO: Implement required visitors
+
