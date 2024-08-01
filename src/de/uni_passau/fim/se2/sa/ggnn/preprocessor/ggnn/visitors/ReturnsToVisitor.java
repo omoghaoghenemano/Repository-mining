0cb1665ce692dea4101de60597b61d50d2bffab1
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-01 21:53:04 +0200

    add return visitor

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/ReturnsToVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/ReturnsToVisitor.java
@@ -1,10 +1,13 @@
 package de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors;
 
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.AstNode;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.declaration.MethodDeclaration;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.ReturnStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.visitor.AstVisitorWithDefaults;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.IdentityWrapper;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.Pair;
 
+import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Set;
 
@@ -20,5 +23,29 @@ public class ReturnsToVisitor implements AstVisitorWithDefaults<Set<Pair<Identit
         this.astNodeMap = astNodeMap;
     }
 
-    // TODO: Implement required visitors
+    @Override
+    public Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> visit(AstNode node, Void arg) {
+        Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> edges = new HashSet<>();
+        if (node instanceof MethodDeclaration) {
+            methodDecl = astNodeMap.get(node);
+            for (AstNode child : node.children()) {
+                edges.addAll(child.accept(this, arg));
+            }
+            methodDecl = null; // Reset after visiting the method
+        } else if (node instanceof ReturnStmt) {
+            IdentityWrapper<AstNode> returnNode = astNodeMap.get(node);
+            if (methodDecl != null && returnNode != null) {
+                edges.add(new Pair<>(returnNode, methodDecl));
+            }
+        } else {
+            for (AstNode child : node.children()) {
+                edges.addAll(child.accept(this, arg));
+            }
+        }
+        return edges;
+    }
+
+    // Implement visit methods for specific node types if necessary
 }
+    // TODO: Implement required visitors
+
