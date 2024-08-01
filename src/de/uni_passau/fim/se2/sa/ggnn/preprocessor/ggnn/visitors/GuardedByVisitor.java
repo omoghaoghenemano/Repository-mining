25cc39c3bcb066d725ecd8e96048938537a3cd88
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-02 00:35:59 +0200

    test for addGuardedByEdges

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
@@ -1,6 +1,7 @@
 package de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors;
 
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.AstNode;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.ForStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.IfStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.WhileStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.visitor.AstVisitorWithDefaults;
@@ -22,65 +23,69 @@ public class GuardedByVisitor implements AstVisitorWithDefaults<Void, Set<Pair<I
     }
 
     @Override
-    public Void defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
-        // Visit all children nodes
+    public Void defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
         for (AstNode child : node.children()) {
-            child.accept(this, data);
+            child.accept(this, result);
         }
         return null;
     }
 
     @Override
-    public Void visit(IfStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
-        IdentityWrapper<AstNode> conditionNode = astNodeMap.get(node.condition());
-
-        if (conditionNode != null) {
-            for (AstNode thenStmt : node.thenStmt().members()) {
-                addGuardedByEdges(conditionNode, thenStmt, data);
-            }
-            if (node.elseStmt() != null) {
-                for (AstNode elseStmt : node.elseStmt().get().members()) {
-                    addGuardedByEdges(conditionNode, elseStmt, data);
+    public Void visit(IfStmt ifStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
+        // Process the guard expression of the `if` statement
+        IdentityWrapper<AstNode> guard = astNodeMap.get(ifStmt.condition());
+        if (guard != null) {
+            // Iterate over variables in the `if` statement's body
+            for (AstNode variable : ifStmt.children()) {
+                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
+                if (variableWrapper != null) {
+                    result.add(new Pair<>(variableWrapper, guard));
                 }
             }
         }
 
-        // Visit children nodes
-        node.condition().accept(this, data);
-        node.thenStmt().accept(this, data);
-        if (node.elseStmt() != null) {
-            node.elseStmt().get().accept(this, data);
-        }
 
         return null;
-        
     }
 
     @Override
-    public Void visit(WhileStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
-        IdentityWrapper<AstNode> conditionNode = astNodeMap.get(node.condition());
-
-        if (conditionNode != null) {
-            for (AstNode bodyStmt : node.block().members()) {
-                addGuardedByEdges(conditionNode, bodyStmt, data);
+    public Void visit(WhileStmt whileStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
+        // Process the guard expression of the `while` statement
+        IdentityWrapper<AstNode> guard = astNodeMap.get(whileStmt.condition());
+        if (guard != null) {
+            // Iterate over variables in the `while` statement's body
+            for (AstNode variable : whileStmt.children()) {
+                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
+                if (variableWrapper != null) {
+                    result.add(new Pair<>(variableWrapper, guard));
+                }
             }
         }
 
-        // Visit children nodes
-        node.condition().accept(this, data);
-        node.block().accept(this, data);
+        // Continue visiting child nodes
 
         return null;
     }
 
-    private void addGuardedByEdges(IdentityWrapper<AstNode> conditionNode, AstNode guardedNode, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
-        IdentityWrapper<AstNode> guardedWrapper = astNodeMap.get(guardedNode);
-        if (guardedWrapper != null) {
-            data.add(new Pair<>(conditionNode, guardedWrapper));
-            // Visit the guarded node to continue traversal
-            guardedNode.accept(this, data);
+// Implement to be done similar methods for other relevant node types (e.g., `ForStmt`, `DoWhileStmt`, `CatchClause`, `TernaryExpr`, `SwitchStmt`)
+
+    @Override
+    public Void visit(ForStmt forStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
+        // Process the guard expression of the `for` statement
+        IdentityWrapper<AstNode> guard = astNodeMap.get(forStmt.forControl());
+        if (guard != null) {
+            // Iterate over variables in the `for` statement's body
+            for (AstNode variable : forStmt.children()) {
+                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
+                if (variableWrapper != null) {
+                    result.add(new Pair<>(variableWrapper, guard));
+                }
+            }
         }
+
+        return null;
     }
+
 }
 
     // TODO: Implement required visitors
