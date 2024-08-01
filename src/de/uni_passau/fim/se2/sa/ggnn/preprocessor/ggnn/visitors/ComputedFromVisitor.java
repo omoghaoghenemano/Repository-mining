cc7e5bc945d14341c2e46477b06bf4e768d5c150
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-01 23:32:31 +0200

    test for ComputedFromVisitor

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/ComputedFromVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/ComputedFromVisitor.java
@@ -21,11 +21,14 @@ public class ComputedFromVisitor implements
     public ComputedFromVisitor(IdentityHashMap<AstNode, IdentityWrapper<AstNode>> astNodeMap) {
         this.astNodeMap = astNodeMap;
     }
+    @Override
     public Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
+        // Default action to handle other nodes
+        for (AstNode child : node.children()) {
+            child.accept(this, data);
+        }
         return data;
     }
-
-
 
     @Override
     public Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> visit(AssignmentExpr node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> data) {
@@ -38,12 +41,14 @@ public class ComputedFromVisitor implements
             computedFrom.add(new Pair<>(rightNode, leftNode)); // right side is the source, left side is the result
         }
 
+        // Visit children nodes to ensure full traversal
+        node.left().accept(this, data);
+        node.right().accept(this, data);
         data.addAll(computedFrom);
         return data;
     }
 
     // Implement other visit methods for different node types if needed
 }
-
     // TODO: Implement required visitors
 
