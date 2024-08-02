b92a8c6e287879cd84e1b825e06cca3adcd0bd91
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-02 12:37:12 +0200

    add gnnedge

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/GGNNGraphBuilder.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/GGNNGraphBuilder.java
@@ -9,7 +9,12 @@ import de.uni_passau.fim.se2.sa.ggnn.ast.model.declaration.MethodDeclaration;
 import de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors.GGNNEdgesVisitor;
 import de.uni_passau.fim.se2.sa.ggnn.program_graphs.Builder;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.IdentityWrapper;
+import de.uni_passau.fim.se2.sa.ggnn.util.functional.Pair;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -26,10 +31,36 @@ public class GGNNGraphBuilder implements Builder<MethodDeclaration, GGNNGraph> {
      */
     @Override
     public GGNNGraph build(final MethodDeclaration method) {
-        // TODO Implement me
-        throw new UnsupportedOperationException("Implement me");
+        // Instantiate the GGNNEdgesVisitor
+        GGNNEdgesVisitor edgesVisitor = new GGNNEdgesVisitor(method);
+        // Get the edges from the visitor
+        Map<GGNNEdgeType, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>>> edgeTypeSetMap = edgesVisitor.getEdges();
 
+        // Create the directed graph
+        Graph<IdentityWrapper<AstNode>, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+        // Add all vertices (nodes) to the graph
+        edgeTypeSetMap.values().forEach(pairs -> {
+            pairs.forEach(pair -> {
+                graph.addVertex(pair.a());
+                graph.addVertex(pair.b());
+            });
+        });
+
+        // Add all edges to the graph
+        edgeTypeSetMap.values().forEach(pairs -> {
+            pairs.forEach(pair -> {
+                graph.addEdge(pair.a(), pair.b());
+            });
+        });
+
+        // Get the label nodes (in this case, it's just the method name)
+        Set<IdentityWrapper<AstNode>> labelNodes = getLabelNodes(method);
+
+        // Create and return the GGNNGraph
+        return new GGNNGraph(method.name().toString(), labelNodes, graph, edgeTypeSetMap);
     }
+
 
     private Set<IdentityWrapper<AstNode>> getLabelNodes(final MethodDeclaration method) {
         return Set.of(IdentityWrapper.of(method.name()));
