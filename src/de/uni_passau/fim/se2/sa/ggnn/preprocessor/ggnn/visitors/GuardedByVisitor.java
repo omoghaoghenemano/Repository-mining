87f14afd06a3e89b24d345f5379019df3c1c9041
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-02 08:15:27 +0200

    test for addGuardedByEdges

--- src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/preprocessor/ggnn/visitors/GuardedByVisitor.java
@@ -1,9 +1,11 @@
 package de.uni_passau.fim.se2.sa.ggnn.preprocessor.ggnn.visitors;
 
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.AstNode;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.DoWhileStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.ForStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.IfStmt;
 import de.uni_passau.fim.se2.sa.ggnn.ast.model.statement.WhileStmt;
+import de.uni_passau.fim.se2.sa.ggnn.ast.model.switch_node.Switch;
 import de.uni_passau.fim.se2.sa.ggnn.ast.visitor.AstVisitorWithDefaults;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.IdentityWrapper;
 import de.uni_passau.fim.se2.sa.ggnn.util.functional.Pair;
@@ -23,70 +25,77 @@ public class GuardedByVisitor implements AstVisitorWithDefaults<Void, Set<Pair<I
     }
 
     @Override
-    public Void defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
-        for (AstNode child : node.children()) {
-            child.accept(this, result);
-        }
+    public Void visit(ForStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        // Extract the guard expression for the ForStmt
+        AstNode guard = node.statementBlock();
+        addGuardedByPair(node, guard, arg);
+        // Visit children
+        node.children().forEach(child -> child.accept(this, arg));
         return null;
     }
 
     @Override
-    public Void visit(IfStmt ifStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
-        // Process the guard expression of the `if` statement
-        IdentityWrapper<AstNode> guard = astNodeMap.get(ifStmt.condition());
-        if (guard != null) {
-            // Iterate over variables in the `if` statement's body
-            for (AstNode variable : ifStmt.children()) {
-                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
-                if (variableWrapper != null) {
-                    result.add(new Pair<>(variableWrapper, guard));
-                }
-            }
-        }
-
-
+    public Void visit(IfStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        // Extract the guard expression for the IfStmt
+        AstNode guard = node.condition();
+        addGuardedByPair(node, guard, arg);
+        // Visit children
+        node.children().forEach(child -> child.accept(this, arg));
         return null;
     }
 
     @Override
-    public Void visit(WhileStmt whileStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
-        // Process the guard expression of the `while` statement
-        IdentityWrapper<AstNode> guard = astNodeMap.get(whileStmt.condition());
-        if (guard != null) {
-            // Iterate over variables in the `while` statement's body
-            for (AstNode variable : whileStmt.children()) {
-                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
-                if (variableWrapper != null) {
-                    result.add(new Pair<>(variableWrapper, guard));
-                }
-            }
-        }
-
-        // Continue visiting child nodes
-
+    public Void visit(WhileStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        // Extract the guard expression for the WhileStmt
+        AstNode guard = node.condition();
+        addGuardedByPair(node, guard, arg);
+        // Visit children
+        node.children().forEach(child -> child.accept(this, arg));
         return null;
     }
-
-// Implement to be done similar methods for other relevant node types (e.g., `ForStmt`, `DoWhileStmt`, `CatchClause`, `TernaryExpr`, `SwitchStmt`)
 
     @Override
-    public Void visit(ForStmt forStmt, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> result) {
-        // Process the guard expression of the `for` statement
-        IdentityWrapper<AstNode> guard = astNodeMap.get(forStmt.forControl());
-        if (guard != null) {
-            // Iterate over variables in the `for` statement's body
-            for (AstNode variable : forStmt.children()) {
-                IdentityWrapper<AstNode> variableWrapper = astNodeMap.get(variable);
-                if (variableWrapper != null) {
-                    result.add(new Pair<>(variableWrapper, guard));
-                }
-            }
-        }
-
+    public Void visit(DoWhileStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        // Extract the guard expression for the WhileStmt
+        AstNode guard = node.condition();
+        addGuardedByPair(node, guard, arg);
+        // Visit children
+        node.children().forEach(child -> child.accept(this, arg));
         return null;
     }
 
+    @Override
+    public Void visit(Switch.SwitchStmt node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        // Extract the guard expression for the WhileStmt
+        AstNode guard = node.check();
+        addGuardedByPair(node, guard, arg);
+        // Visit children
+        node.children().forEach(child -> child.accept(this, arg));
+        return null;
+    }
+
+
+
+    private void addGuardedByPair(AstNode node, AstNode guard, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        if (guard != null) {
+            IdentityWrapper<AstNode> wrappedNode = astNodeMap.get(node);
+            IdentityWrapper<AstNode> wrappedGuard = astNodeMap.get(guard);
+            if (wrappedNode != null && wrappedGuard != null) {
+                arg.add(new Pair<>(wrappedNode, wrappedGuard));
+            }
+        }
+    }
+
+    @Override
+    public Void defaultAction(AstNode node, Set<Pair<IdentityWrapper<AstNode>, IdentityWrapper<AstNode>>> arg) {
+        node.children().forEach(child -> child.accept(this, arg));
+        return null;
+    }
 }
+
+
+
+
 
     // TODO: Implement required visitors
 
