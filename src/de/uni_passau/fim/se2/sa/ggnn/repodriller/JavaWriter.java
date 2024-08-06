6a5cc19aab83a4a007c97a6dd1bf6ec0d59282ec
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-08-06 17:22:17 +0200

    modify javawrite

--- src/de/uni_passau/fim/se2/sa/ggnn/repodriller/JavaWriter.java
+++ src/de/uni_passau/fim/se2/sa/ggnn/repodriller/JavaWriter.java
@@ -2,17 +2,16 @@ package de.uni_passau.fim.se2.sa.ggnn.repodriller;
 
 import org.repodriller.persistence.PersistenceMechanism;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class JavaWriter implements PersistenceMechanism {
 
     private final Path directory;
     private String fileName;
-    private FileWriter writer;
+    private BufferedWriter writer;
 
     public JavaWriter(Path directory) {
         this.directory = directory;
@@ -20,44 +19,33 @@ public class JavaWriter implements PersistenceMechanism {
 
     @Override
     public synchronized void write(Object... objects) {
-        try {
-            ensureDirectoryExists();
+        if (writer == null) {
+            throw new IllegalStateException("Writer not initialized. Call setFileName() before write().");
+        }
 
-            String content = extractContent(objects);
+
+        try {
+            String content;
+
             if (objects.length > 1) {
-                setFileName((String) objects[1]);
+                content = (String) objects[2];
+                setFileName(((String) objects[1]));
+            } else {
+                content = (String) objects[0];
             }
 
-            writeContentToFile(content);
+                writer.write(content);
+                writer.newLine();
+
+
+            writer.flush();
+
 
         } catch (IOException e) {
-            throw new RuntimeException("Error writing file: " + e.getMessage(), e);
+            throw new RuntimeException("Error writing data", e);
         }
     }
 
-    private void ensureDirectoryExists() throws IOException {
-        if (!Files.exists(directory)) {
-            Files.createDirectories(directory);
-        }
-    }
-
-    private String extractContent(Object... objects) {
-        if (objects.length > 1) {
-            return (String) objects[2];
-        } else {
-            return (String) objects[0];
-        }
-    }
-
-    private void writeContentToFile(String content) throws IOException {
-        Path filePath = directory.resolve(fileName.replace("/", "_"));
-        Files.createDirectories(filePath.getParent());
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))) {
-            writer.write(content);
-            writer.newLine();
-        }
-    }
 
 
     @Override
@@ -66,14 +54,26 @@ public class JavaWriter implements PersistenceMechanism {
             try {
                 writer.close();
             } catch (IOException e) {
-                throw new RuntimeException("Error closing file writer: " + e.getMessage(), e);
+                throw new RuntimeException("Error closing writer", e);
             }
         }
     }
 
     public void setFileName(String fileName) {
+        close();
         this.fileName = fileName;
+        initializeWriter();
+    }
+
+    private void initializeWriter() {
+        try {
+            if (!Files.exists(directory)) {
+                Files.createDirectories(directory);
+            }
+            Path filePath = directory.resolve(fileName);
+            writer = Files.newBufferedWriter(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing writer", e);
+        }
     }
 }
-
-
